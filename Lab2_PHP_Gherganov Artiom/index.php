56d1b93c31c03a73ce8b569771a1500c87f1c19f
Artimon4ik <artiom.gherganov@gmail.com>
2026-03-17 10:40:09 +0200

    Add files via upload

---
+++ Lab2_PHP_Gherganov Artiom/index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+echo "Привет, мир!<br />";
+
+ echo "Hello, World with echo!<br />";
+
+print "Hello, World with print!<br />";
+*/
+
+//Создание переменных
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+
+//Конкатинация
+echo  "Число дней:" . $days ." <br />";
+echo "Сообщение работникам:". $message . "<br />";
+
+//Кавычки
+echo "Число дней: $days<br />";
+echo "Сообщение: $message<br />";
+
+?>
+
---
+++ Lab3_PHP_Gherganov Artiom/readme.md
@@ -0,0 +1,13 @@
+### Добавьте вывод промежуточных значений `$a` и `$b` на каждом шаге цикла.
+![alt text](image.png)
+
+
+### Перепишите этот цикл, используя оператор while и do-while.
+
+- c while
+
+![alt text](image-1.png)
+
+- с do-while
+
+![alt text](image-2.png)
